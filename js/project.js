2b729926d33fcca43d09970a354356b2930bd33d
riyan <riyanmaulana04@gmail.com>
2024-01-04 00:06:19 +0700

    pengkondisian durasi waktu

--- js/project.js
+++ js/project.js
@@ -8,6 +8,45 @@ function addData(event) {
   let startDate = document.getElementById("start-date").value;
   let endDate = document.getElementById("end-date").value;
 
+  let startDateValue = new Date(startDate);
+  let endDateValue = new Date(endDate);
+
+  let durasiWaktu = endDateValue.getTime() - startDateValue.getTime();
+  let durasiHari = durasiWaktu / (1000 * 3600 * 24);
+  let durasiMinggu = Math.floor(durasiHari / 7);
+  let sisaHari = durasiHari % 7;
+
+  let durasiBulan = Math.floor(durasiHari / 30);
+  let sisaHariBulan = durasiHari % 30;
+
+  let durasiTahun = Math.floor(durasiHari / 365);
+  let sisaHariTahun = durasiHari % 365;
+
+  let durasiProject = "";
+
+  if (durasiHari <= 6) {
+    durasiProject = ` Durasi ${durasiHari + 1} Hari`;
+  } else if (durasiTahun > 0) {
+    if (sisaHariTahun > 0) {
+      durasiProject = `Durasi ${durasiTahun} Tahun ${Math.floor(
+        sisaHariTahun / 30
+      )} Bulan ${Math.floor(sisaHariTahun % 30)} Hari`;
+    } else {
+      durasiProject = `Durasi ${durasiTahun} Tahun`;
+    }
+  } else if (durasiBulan > 0) {
+    if (sisaHariBulan > 0) {
+      durasiProject = `Durasi ${durasiBulan} Bulan ${sisaHari - 1} Hari`;
+    } else {
+      durasiProject = `Durasi ${durasiBulan} Bulan`;
+    }
+  } else if (sisaHari > 0) {
+    durasiProject = `Durasi ${durasiMinggu} Minggu ${sisaHari} Hari`;
+  } else {
+    durasiProject = `Durasi ${durasiMinggu} Minggu`;
+  }
+  console.log(durasiProject);
+
   //icon
   const golang = `<i class="fa-brands fa-golang"></i>`;
   const java = ` <i class="fa-brands fa-java"></i>`;
@@ -27,6 +66,7 @@ function addData(event) {
     title,
     startDate,
     endDate,
+    durasiProject,
     description,
     image,
     getIcon1,
@@ -56,7 +96,7 @@ function renderProject() {
       <div class="elemen">
         <a href="/views/detai.html">${dataProject[index].title}</a>
         <div class="kelas4">
-          <p>${dataProject[index].postAt} | ${dataProject[index].author}</p>
+          <p>${dataProject[index].postAt} | ${dataProject[index].durasiProject} | ${dataProject[index].author}</p>
         </div>
 
         <p class="lorem">
